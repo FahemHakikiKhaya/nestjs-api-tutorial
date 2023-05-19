2a12366f6a86e21f419652738c13e7a7ddeee39c
FahemHakikiKhaya <hakikihaya@gmail.com>
2023-05-19 13:39:08 +0700

    feat: prisma setup

---
+++ prisma/migrations/20230519055127_update_user_and_create_bookmark/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `password` VARCHAR(191) NOT NULL,
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
+    MODIFY `name` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Bookmark` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `link` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/auth/app.module.ts
+++ src/auth/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from 'module';
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({})
-class AuthModules {}
+@Module({ controllers: [AuthController], providers: [AuthService] })
+export class AuthModules {}
